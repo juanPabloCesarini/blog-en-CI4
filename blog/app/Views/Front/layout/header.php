b3a51f1ab3f37e0617a208ecf14608b8efe0b18f
juanPabloCesarini <juanpcesarini@hotmail.com>
2024-01-20 21:02:54 -0300

    layout

---
+++ blog/app/Views/Front/layout/header.php
@@ -0,0 +1,10 @@
+ <section class="hero is-primary">
+      <div class="hero-body">
+           <p class="title">
+                Mi Blob personal
+           </p>
+           <p class="subtitle">
+                Lista de entradas
+           </p>
+      </div>
+ </section>
